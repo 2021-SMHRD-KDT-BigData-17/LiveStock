6622351fdc3c02f1e0c48119d0e475a8cfa95e56
smhrds <smhrds@smhrd>
2023-05-31 14:45:45 +0900

    data

--- 2023LiveStock/src/main/java/kr/or/livestock/controller/m_controller.java
+++ 2023LiveStock/src/main/java/kr/or/livestock/controller/m_controller.java
@@ -2,6 +2,7 @@ package kr.or.livestock.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import kr.or.livestock.mapper.user_info_mapper;
 
@@ -11,5 +12,12 @@ public class m_controller {
 	@Autowired
 	user_info_mapper mapper;
 	
+	@RequestMapping("/login")
+	public String login() {
+		
+		mapper.sign_in();
+		
+		return "";
+	}
 	
 }
--- 2023LiveStock/src/main/java/kr/or/livestock/mapper/user_info_mapper.java
+++ 2023LiveStock/src/main/java/kr/or/livestock/mapper/user_info_mapper.java
@@ -2,7 +2,13 @@ package kr.or.livestock.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import kr.or.livestock.entity.user_info;
+
 @Mapper
 public interface user_info_mapper {
+
+	public user_info sign_in();
+	
+	public int sign_up();
 	
 }
